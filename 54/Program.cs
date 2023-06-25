dd3c4150a52c242cd773ee5b506cd800985e9275
captain-constantine-margarynt <89223974+captain-constantine-margarynt@users.noreply.github.com>
2023-06-25 20:17:35 +0300

    HW8.Task 54

---
+++ 54/Program.cs
@@ -0,0 +1,96 @@
+﻿/* Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию 
+элементы каждой строки двумерного массива.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+В итоге получается вот такой массив:
+7 4 2 1
+9 5 3 2
+8 4 4 2
+*/
+
+using System;
+using static System.Console;
+
+Clear();
+
+Write("Введите количество строк массива: ");
+int rows = int.Parse(ReadLine());
+Write("Введите количество столбцов массива: ");
+int columns = int.Parse(ReadLine());
+int[,] array = GetArray(rows, columns, 0, 10);
+Write("Сгенерированный массив: ");
+WriteLine();
+PrintArray(array);
+WriteLine();
+
+
+int[,] sorted = SortArray(array);
+Write("Отсортированный массив: ");
+WriteLine();
+PrintArray(sorted);
+WriteLine();
+
+
+
+int[,] GetArray(int m, int n, int min, int max)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(min, max + 1);
+        }
+
+    }
+    return result;
+}
+
+int[] GetRowArray(int[,] inArray)
+{
+    int[] result = new int[inArray.GetLength(0) * inArray.GetLength(1)];
+    int index = 0;
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            result[index] = inArray[i, j];
+            index++;
+        }
+    }
+    return result;
+}
+
+int [,] SortArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(1)-1; i++)
+    {
+        for (int j = i + 1; j < inArray.GetLength(0)-1; j++)
+        {
+            if (inArray[i,j] < inArray[i,j+1])
+            {
+                int k = inArray[i, j];
+                inArray[i , j] = inArray[i , j + 1];
+                inArray[i, j + 1] = k;
+            }
+        }
+    }
+    return inArray;
+}
+
+
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Write($"{inArray[i, j]} ");
+        }
+        WriteLine();
+    }
+}
+
+
